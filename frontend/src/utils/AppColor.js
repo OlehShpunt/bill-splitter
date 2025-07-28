79817716f59d92a03ed72ba257c66a436907a9ac
OlehShpunt <olehshpunt2@gmail.com>
2025-07-28 23:57:11 +0200

    feat: add AppColor and Border utils

---
+++ frontend/src/utils/AppColor.js
@@ -0,0 +1,22 @@
+const AppColor = {
+  background: {
+    WHITE: "bg-app-white",
+    LIGHT: "bg-app-light",
+    REGULAR: "bg-app-regular",
+    DARK: "bg-app-dark",
+  },
+  border: {
+    WHITE: "border-app-white",
+    LIGHT: "border-app-light",
+    REGULAR: "border-app-regular",
+    DARK: "border-app-dark",
+  },
+  text: {
+    WHITE: "text-app-white",
+    LIGHT: "text-app-light",
+    REGULAR: "text-app-regular",
+    DARK: "text-app-dark",
+  },
+};
+
+export default AppColor;
---
+++ frontend/src/utils/Border.js
@@ -0,0 +1,8 @@
+const Border = {
+  ACTIVE: "border-2",
+  PASSIVE: "border-1",
+  NONE: "",
+  THICKEST: "border-3",
+};
+
+export default Border;
