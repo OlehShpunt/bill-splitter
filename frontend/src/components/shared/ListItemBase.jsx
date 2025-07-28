59dc7a135ce8308c30e0ac2ad24cb57b0a9b59b6
OlehShpunt <olehshpunt2@gmail.com>
2025-07-28 23:59:05 +0200

    feat: add ListItemBase (UI)

---
+++ frontend/src/components/shared/ListItemBase.jsx
@@ -0,0 +1,22 @@
+import AppColor from "../../utils/AppColor";
+import Border from "../../utils/Border";
+
+export default function ListItemBase({
+  name = "Not specified",
+  value = "0.00",
+  fillColor = AppColor.background.WHITE,
+  borderColor = AppColor.border.LIGHT,
+  borderWidth = Border.PASSIVE,
+  textColor = AppColor.text.DARK,
+}) {
+  return (
+    <div
+      className={`m-auto my-2 max-w-100 w-297/316 h-20 rounded-[10] ${fillColor} ${borderColor} ${borderWidth} ${textColor}`}
+    >
+      <div className="flex justify-between items-center px-4 h-full">
+        <span id="name">{name.toString()}</span>
+        <span id="value">{value.toString()}</span>
+      </div>
+    </div>
+  );
+}
