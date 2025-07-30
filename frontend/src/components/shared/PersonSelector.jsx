a2ee3dad814f6fb8a6355a3bd64805ae4659e26d
OlehShpunt <olehshpunt2@gmail.com>
2025-07-30 13:55:32 +0200

    fix: resolve vertical misalignment of empty PersonSelector due to baseline shift
    When adding empty and full PersonSelectors together under one parent, PersonSelector with text shifts down, while PersonSelector without text stays in place

--- frontend/src/components/shared/PersonSelector.jsx
+++ frontend/src/components/shared/PersonSelector.jsx
@@ -1,5 +1,6 @@
 import AppColor from "../../utils/AppColor";
 import Border from "../../utils/Border";
+import plusImage from "../../../public/plus.png";
 
 export default function PersonSelector({
   text = null,
@@ -7,12 +8,16 @@ export default function PersonSelector({
   borderColor = AppColor.border.LIGHT,
   borderWidth = Border.PASSIVE,
   textColor = AppColor.text.REGULAR,
+  children = null,
 }) {
   return (
-    <span
-      className={`inline-flex items-center justify-center mx-1 my-2 rounded-full text-3xl h-18 w-18 ${backgroundColor} ${borderColor} ${borderWidth} ${textColor}`}
-    >
-      {text}
+    <span className="inline-flex items-center gap-2 align-middle">
+      <span
+        className={`inline-flex items-center justify-center mx-1 my-2 rounded-full text-3xl h-18 w-18 ${backgroundColor} ${borderColor} ${borderWidth} ${textColor}`}
+      >
+        {text}
+        {children}
+      </span>
     </span>
   );
 }
