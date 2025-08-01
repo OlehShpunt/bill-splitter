d1ccdcc2c323ee1748386ce35fd7dc56078b89bc
OlehShpunt <olehshpunt2@gmail.com>
2025-08-01 23:59:01 +0200

    feat: fetch ItemList data in ScanConfirmPage
    Sends the image to the backend and gets the extracted items, which are passed to ItemList in a single JSON object for rendering.

--- frontend/src/app/scan/confirm/page.jsx
+++ frontend/src/app/scan/confirm/page.jsx
@@ -1,17 +1,39 @@
-import Item from "@/components/shared/ListItemBase.jsx";
-import AppColor from "@/utils/AppColor";
-import Border from "@/utils/Border";
+import Button from "@/components/shared/Button";
+import Header from "@/components/shared/Header";
+import ItemList from "@/components/shared/ItemList";
 
-export default function ScanConfirmPage() {
+export default async function ScanConfirmPage() {
+  let itemListData;
+
+  try {
+    // TODO: use Context to save the image url in ScanPage and dynamically pass it below
+    const imageUrl = encodeURIComponent("https://i.redd.it/43vg6ll5hz2b1.jpg");
+
+    // Send the image to the backend and get the extracted items
+    const response = await fetch(
+      `http://127.0.0.1:8000/?image_url=${imageUrl}`,
+      {
+        cache: "default",
+      }
+    );
+
+    // Convert the response into a JSON object
+    const dataJsonString = await response.json();
+    const dataJson = JSON.parse(dataJsonString);
+    itemListData = dataJson;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return;
+  }
+
   return (
     <>
-      <Item></Item>
-      <Item
-        name="test"
-        value="10.0"
-        fillColor={AppColor.background.DARK}
-      ></Item>
-      <Item></Item>
+      <Header
+        heading="Confirm item list"
+        description="Click an item to correct price or name"
+      ></Header>
+      <ItemList items={itemListData}></ItemList>
+      <Button text="Confirm"></Button>
     </>
   );
 }
