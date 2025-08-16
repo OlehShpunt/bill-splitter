beb5c0080146f5da78dd6ee66ae1b09275f73446
OlehShpunt <olehshpunt2@gmail.com>
2025-08-16 17:10:50 +0200

    chore: rename person component for clarity

--- frontend/src/components/shared/Person.jsx
+++ frontend/src/components/shared/Person.jsx
@@ -1,9 +1,8 @@
 import AppColor from "../../utils/AppColor";
 import Border from "../../utils/Border";
-import plusImage from "../../../public/plus.png";
 
-export default function PersonSelector({
-  text = null,
+export default function Person({
+  name = "",
   backgroundColor = AppColor.background.WHITE,
   borderColor = AppColor.border.LIGHT,
   borderWidth = Border.PASSIVE,
@@ -15,21 +14,21 @@ export default function PersonSelector({
       <span
         className={`inline-flex items-center justify-center mx-1 my-2 rounded-full text-3xl h-18 w-18 ${backgroundColor} ${borderColor} ${borderWidth} ${textColor}`}
       >
-        {text}
+        {name.slice(0, 1).toUpperCase()}
         {children}
       </span>
     </span>
   );
 }
 
-export function AddPersonSelector() {
+export function PlusPerson() {
   return (
-    <PersonSelector
+    <Person
       backgroundColor={AppColor.background.WHITE}
       borderColor={AppColor.border.LIGHT + " border-dashed"}
       textColor={AppColor.text.LIGHT + " text-5xl font-light"}
     >
       <span className="-translate-y-0.5">+</span>
-    </PersonSelector>
+    </Person>
   );
 }
