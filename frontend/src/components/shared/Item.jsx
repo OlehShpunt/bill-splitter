989eec58464a6ecff92271b23d311804a301a326
OlehShpunt <olehshpunt2@gmail.com>
2025-08-16 17:09:18 +0200

    feat: change Item state and appearance on click, if currentPerson is defined

--- frontend/src/components/shared/Item.jsx
+++ frontend/src/components/shared/Item.jsx
@@ -3,11 +3,13 @@ import ItemState from "@/utils/ItemState";
 export default function Item({
   name = "Not specified",
   value = "0.00",
-  itemState = ItemState.NORMAL,
+  state = ItemState.NORMAL,
+  onClick = () => {},
 }) {
   return (
     <div
-      className={`m-auto my-2 max-w-100 w-297/316 h-18 rounded-[10] ${itemState}`}
+      className={`m-auto my-2 max-w-100 w-297/316 h-18 rounded-[10] ${state}`}
+      onClick={onClick}
     >
       <div className="flex justify-between items-center px-4 h-full">
         <span id="name">{name.toString()}</span>
--- frontend/src/components/shared/ListOfItems.jsx
+++ frontend/src/components/shared/ListOfItems.jsx
@@ -1,19 +1,71 @@
+import { useState } from "react";
 import Item from "./Item";
+import ItemState from "@/utils/ItemState";
 
-export default function ListOfItems({ items = [] }) {
-  let i = 0;
+export default function ListOfItems({ items = [], currentPerson = null }) {
+  const [selections, setSelections] = useState({});
+
+  // Changes item state when clicked
+  const onItemClicked = (itemKey) => {
+    if (!currentPerson) {
+      return;
+    }
+
+      const currentSelectedItemsArray = previousSelections[currentPerson] || [];
+      const index = currentSelectedItemsArray.indexOf(itemKey);
+      if (index > -1) {
+        // Already selected, so remove it
+        // NOTE: currentSelectedItemsArray can't be mutated, so simply currentSelectedItemsArray.splice(itemKey, 1) won't work
+        const newCurrentSelectedItemsArray = [
+          ...currentSelectedItemsArray.slice(0, index),
+          ...currentSelectedItemsArray.slice(index + 1),
+        ];
+        return {
+          ...previousSelections,
+          [currentPerson]: newCurrentSelectedItemsArray,
+        };
+      } else {
+        // Not selected, so add it
+        // NOTE: currentSelectedItemsArray can't be mutated, so simply currentSelectedItemsArray.push(itemKey) won't work
+        const newCurrentSelectedItemsArray = [
+          ...currentSelectedItemsArray,
+          itemKey,
+        ];
+        return {
+          ...previousSelections,
+          [currentPerson]: newCurrentSelectedItemsArray,
+        };
+      }
+    });
+  };
+
   const listItems = items.map((item) => {
+    let itemState;
+    // If the item is selected by the current person, set it to activated
+    if (selections[currentPerson]?.includes(item.key)) {
+      itemState = ItemState.ACTIVATED;
+    }
+    // If the item is selected by any other person, set it to deactivated
+    else if (
+      Object.values(selections).some((array) => array.includes(item.key))
+    ) {
+      itemState = ItemState.DEACTIVATED;
+    }
+    // Otherwise, set it to normal
+    else {
+      itemState = ItemState.NORMAL;
+    }
+
     return (
       <Item
-        key={item.key ? item.key : i++}
+        key={item.key}
         name={item.name}
         value={item.value}
-        backgroundColor={item.backgroundColor}
-        borderColor={item.borderColor}
-        borderWidth={item.borderWidth}
-        textColor={item.textColor}
+        onClick={() => onItemClicked(item.key)}
+        state={itemState}
       ></Item>
     );
   });
+
   return listItems;
 }
